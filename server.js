8c3b9b96cbd278e89841921dc912439b4e045603
Jassim Hayat <jassim.hayat@gmail.com>
2025-01-07 21:00:19 +0300

    cookbook-lab

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+
 const dotenv = require('dotenv');
 dotenv.config();
 const express = require('express');
@@ -7,7 +8,12 @@ const methodOverride = require('method-override');
 const morgan = require('morgan');
 const session = require('express-session');
 
+const isSignedIn = require('./middleware/is-signed-in.js');
+const passUserToView = require('./middleware/pass-user-to-view.js');
+
 const authController = require('./controllers/auth.js');
+const foodsController = require('./controllers/foods.js');
+
 
 const port = process.env.PORT ? process.env.PORT : '3000';
 
@@ -42,7 +48,14 @@ app.get('/vip-lounge', (req, res) => {
   }
 });
 
+app.use(passUserToView);
 app.use('/auth', authController);
+app.use(isSignedIn);
+app.use('/users/:userId/foods', foodsController);
+
+
+
+
 
 app.listen(port, () => {
   console.log(`The express app is ready on port ${port}!`);
